004b4989a5875ef61f643d9aa8c6397ceb2d645c
Mahfuz Redwan <mredwan.kemal@gmail.com>
2024-12-14 09:01:00 +0300

    updated the hamburger menu eith a bouce animation

--- JS/app.js
+++ JS/app.js
@@ -7,18 +7,18 @@ burger.addEventListener("click",(e)=>{
     let line2 = burgerLines[1];
     let line3 = burgerLines[2];
     const lineTl = gsap.timeline({
-        defaults: {duration:0.8, ease:"power2.inout"}
+        defaults: {duration:0.8, ease:"bounce.out"}
     });
     if (!burger.classList.contains("burger-active")) {
         burger.classList.add("burger-active");
         lineTl.to(line1,{rotate: "45deg", y: "-50%",});
-        lineTl.to(line2,{x: "100%"},"-=0.8");
-        lineTl.to(line3,{rotate: "-45deg", y: "50%",},"-=0.8");
+        lineTl.to(line2,{x: "100%",opacity:0},"-=0.8");
+        lineTl.to(line3,{rotate: "-45deg", y: "50%",},"-=1");
     }else{
         burger.classList.remove("burger-active");
         lineTl.to(line1,{rotate: "0deg", y: 0});
-        lineTl.to(line2,{x: "0%"},"-=0.8");
-        lineTl.to(line3,{rotate: "0deg", y: 0},"-=0.8");
+        lineTl.to(line2,{x: "0%" ,opacity:1},"-=0.9");
+        lineTl.to(line3,{rotate: "0deg", y: 0},"-=1");
     }
     
 })
