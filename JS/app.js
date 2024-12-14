f3cbdd9a399d5188efb5d8eb78e03296ed899f3a
Mahfuz Redwan <mredwan.kemal@gmail.com>
2024-12-14 07:32:35 +0300

    added the toogle animation to the burger menu for the active and inactive state using gsap

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,24 @@
 // animatins for the burger menu
+let burger =document.querySelector(".toggle-btn");
+let burgerLines = document.querySelectorAll(".toggle-btn svg line");
 
-let burger = document.querySelector(".toggle-btn svg");
-console.log(burger.childNodes);
+burger.addEventListener("click",(e)=>{
+    let line1 = burgerLines[0];
+    let line2 = burgerLines[1];
+    let line3 = burgerLines[2];
+    const lineTl = gsap.timeline({
+        defaults: {duration:0.8, ease:"power2.inout"}
+    });
+    if (!burger.classList.contains("burger-active")) {
+        burger.classList.add("burger-active");
+        lineTl.to(line1,{rotate: "45deg", y: "-50%",});
+        lineTl.to(line2,{x: "100%"},"-=0.8");
+        lineTl.to(line3,{rotate: "-45deg", y: "50%",},"-=0.8");
+    }else{
+        burger.classList.remove("burger-active");
+        lineTl.to(line1,{rotate: "0deg", y: 0});
+        lineTl.to(line2,{x: "0%"},"-=0.8");
+        lineTl.to(line3,{rotate: "0deg", y: 0},"-=0.8");
+    }
+    
+})
